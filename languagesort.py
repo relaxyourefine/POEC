f322b4e9aa9fcbf4d82a70c1fb7059827b78e384
relaxyourefine <relaxyourefine@pop-os.localdomain>
2023-11-19 20:31:47 -0500

    adding shit

--- languagesort.py
+++ languagesort.py
@@ -7,20 +7,14 @@ import xlsxwriter
 df = pd.read_excel("/home/relaxyourefine/Documents/PythonProjects/POEC/2023_working_manual_8-28-2023 (2.1).xlsm")
 
 def highlight_cells(value):
-    if detect(str(value)) == 'en':
+    if detect(str(value)) != 'en':
+        print("yes")
         return 'background-color: yellow'
     else:
         return 'background-color: white'
 
 styled_df = df.style.applymap(highlight_cells, subset=['abstract'])
 
-
-writer = pd.ExcelWriter('Ver2.xlsx', engine='xlsxwriter')
-
-styled_df.to_excel(writer, sheet_name='Sheet1', index=False)
-
-workbook  = writer.book
-workbook.filename = 'Ver2.xlsm'
-workbook.add_vba_project('./vbaProject.bin')
-
+writer = pd.ExcelWriter('Ver2.xlsm')
+styled_df.to_excel(writer,engine="io.excel.xlsm.writer")
 writer.save()
