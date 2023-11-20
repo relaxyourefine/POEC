8fc9163b3813afc76e6be709b89474057b71bbb0
relaxyourefine <relaxyourefine@pop-os.localdomain>
2023-11-19 20:02:31 -0500

    this is the colored version of xcel

--- languagesort.py
+++ languagesort.py
@@ -1,17 +1,26 @@
 import pandas as pd
 from langdetect import detect
 import xlrd
+import xlsxwriter
 
 
 df = pd.read_excel("/home/relaxyourefine/Documents/PythonProjects/POEC/2023_working_manual_8-28-2023 (2.1).xlsm")
 
+def highlight_cells(value):
+    if detect(str(value)) == 'en':
+        return 'background-color: yellow'
+    else:
+        return 'background-color: white'
 
-def highlight_cells():
-    abstracts = df["abstract"]
-    for abstract in abstracts:
-        if detect(abstract) == 'en':
-            return['background-color yellow']
-        else:
-            return
+styled_df = df.style.applymap(highlight_cells, subset=['abstract'])
 
-df.style.apply(highlight_cells)
+
+writer = pd.ExcelWriter('Ver2.xlsx', engine='xlsxwriter')
+
+styled_df.to_excel(writer, sheet_name='Sheet1', index=False)
+
+workbook  = writer.book
+workbook.filename = 'Ver2.xlsm'
+workbook.add_vba_project('./vbaProject.bin')
+
+writer.save()
